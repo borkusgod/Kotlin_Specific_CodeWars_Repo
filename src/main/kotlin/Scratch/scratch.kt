0035757c7d9d8fc9c77b42dac341f79de0cdb89a
borkusgod <lightraven.bourque@gmail.com>
2022-10-28 21:00:54 -0300

    Just getting started on Kotlin practice. Love how the challenges get you framed for finding solutions and half the time it's the CodeWars interface that is the obstacle. Lol

--- src/main/kotlin/Scratch/scratch.kt
+++ src/main/kotlin/Scratch/scratch.kt
@@ -1,2 +1,19 @@
 package Scratch
 
+fun main() {
+		val spacesEnd: String = "*****  "
+		val spacesMiddle: String = "*****  *****"
+
+		val se_rep: String = spacesEnd.replace(" ", "")
+		val sm_rep: String = spacesMiddle.replace(" ", "")
+
+		println(spacesEnd.length)
+		println(spacesMiddle.length)
+
+		println(se_rep.length)
+		println(sm_rep.length)
+
+	}
+
+
+
---
+++ src/main/kotlin/kyu8/removingWhiteSpace.kt
@@ -0,0 +1,11 @@
+package kyu8
+
+fun main() {
+	println(noSpace("8 j 8   mBliB8g  imjB8B8  jl  B"))
+	println(noSpace("8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd"))
+	println(noSpace("8aaaaa dddd r     "))
+}
+
+fun noSpace(x: String): String {
+	return x.replace(" ", "")
+}
